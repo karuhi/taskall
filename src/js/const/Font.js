25a7471f8d1e3d8d9943820fe83e64bb4693891e
karuhi <karuhi.dev@gmail.com>
2021-03-10 17:39:34 +0900

    デザイン定義の追加

---
+++ src/js/const/Font.js
@@ -0,0 +1,4 @@
+export default {
+  DEFAULT:
+    'open sans,-apple-system,Helvetica Neue,Helvetica,Open Sans,Arial,PingFang SC,PingFang TC,Hiragino Sans GB,Microsoft Yahei,Microsoft Jhenghei,sans-serif',
+}
